6b4a835909adebdabec18486a528d6a8f309ddfc
dramnani <71577723+dramnani@users.noreply.github.com>
2026-06-08 20:30:22 -0700

    Create manifest.R

---
+++ app/manifest.R
@@ -0,0 +1,21 @@
+# generate_manifest.R
+# Creates manifest.json required by Posit Connect Cloud
+
+# Install rsconnect if needed
+if (!requireNamespace("rsconnect", quietly = TRUE)) {
+  install.packages("rsconnect")
+}
+
+library(rsconnect)
+
+# since app is within the "app" subfolder
+app_dir <- "app"
+
+
+# Generate manifest.json
+rsconnect::writeManifest(
+  appDir = app_dir,
+  force = TRUE
+)
+
+cat("Manifest successfully created in:", normalizePath(app_dir), "\n")
